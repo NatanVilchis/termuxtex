80abfb6ff61e138c6f028763e65d9d89b4c86120
Natan Vilchis <contacto@natanvilchis.org>
2019-03-16 23:13:58 -0600

    Update termuxtex.sh

--- termuxtex.sh
+++ termuxtex.sh
@@ -19,20 +19,8 @@ apt -y install coreutils
 #instalamos los archivos necesarios
 apt -y install texlive
 tlmgr install fontspec hyperref amssymb graphicx apacite enumerate commath amsmath booktabs adjustbox babel babel-spanish  inputenc pythonhighlight listings
-tlmgr install xkeyval collectbox pgfplots lm xcolor pgf
-#creamos la carpeta para el ejemplo
-#mkdir /storage/emulated/0/termuxtex_ejemplo
-#obtenemos el archivo zip con los archivos correspondientes
-#wget https://natanvilchis.org/7a16eccf2bcc6d055c043b8c8a543e566d2c5b310d6e5b9b771e7322df3b59c2.zip -P /storage/emulated/0/termuxtex_ejemplo/
-#cd /storage/emulated/0/termuxtex_ejemplo/
-#descromprimimos el archivo zip
-#unzip 7a16eccf2bcc6d055c043b8c8a543e566d2c5b310d6e5b9b771e7322df3b59c2.zip
-#Eliminamos el archivo zip pues ya de descomprimio
-#rm 7a16eccf2bcc6d055c043b8c8a543e566d2c5b310d6e5b9b771e7322df3b59c2.zip
-#echo "--> Se han instalado los requerimientos necesarios"
-#echo "La carpeta de ejemplo esta en /storage/emulated/0/termuxtex_ejemplo"
-#echo "Por favor cerrar la terminal y volverla a abrir para guardar los cambios. Gracias por la espera."
-#kill -9 $PPID
+tlmgr install xkeyval collectbox pgfplots lm xcolor pgf texliveonfly
+
 mv scripts/lualatex2c $HOME/../usr/bin
 chmod +x $HOME/../usr/bin/lualatex2c
 
