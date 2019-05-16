c1c9e64a1574648b6a3b0d46df0ca4e454ea5fc2
Natan Vilchis <contacto@natanvilchis.org>
2019-05-16 00:27:47 -0500

    Update termuxtex.sh

--- termuxtex.sh
+++ termuxtex.sh
@@ -7,13 +7,7 @@ apt -y install texlive
 tlmgr install fontspec hyperref amssymb graphicx apacite enumerate commath amsmath booktabs adjustbox babel babel-spanish  inputenc pythonhighlight listings xkeyval collectbox pgfplots lm xcolor pgf texliveonfly
 
 mv scripts/lualatex2c $HOME/../usr/bin
-chmod +x $HOME/../usr/bin/lualatex2c
-
-mv scripts/lualatex2ca $HOME/../usr/bin
-chmod +x $HOME/../usr/bin/lualatex2ca
+chmod +x $HOME/../usr/bin/lualatex2
 
 mv scripts/pdflatex2c $HOME/../usr/bin
-chmod +x $HOME/../usr/bin/pdflatex2c
-
-mv scripts/pdflatex2ca $HOME/../usr/bin
-chmod +x $HOME/../usr/bin/pdflatex2ca
+chmod +x $HOME/../usr/bin/pdflatex2
